00edbddad1de24868315d0d056b71bf34549bef0
Emerson reyes <reyesemerson643@gmail.com>
2025-09-13 08:55:32 -0500

    Add file

---
+++ Huerta-directa/src/main/java/com/exe/Huerta_directa/HuertaDirectaApplication.java
@@ -0,0 +1,13 @@
+package com.exe.Huerta_directa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuertaDirectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuertaDirectaApplication.class, args);
+	}
+
+}
---
+++ Huerta-directa/src/test/java/com/exe/Huerta_directa/HuertaDirectaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.Huerta_directa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuertaDirectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
