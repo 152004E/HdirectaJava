3ba9cd272226cc764b8876cb3de97d0d512f8aaa
Yeferson Sanchez <jefersonsnachez@gmail.com>
2025-09-15 13:09:06 -0500

    arreglando dto

--- Huerta-directa/src/main/java/com/exe/Huerta_directa/DTO/ProductDTO.java
+++ Huerta-directa/src/main/java/com/exe/Huerta_directa/DTO/ProductDTO.java
@@ -1,8 +1,12 @@
 package com.exe.Huerta_directa.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 
@@ -13,30 +17,6 @@ public class ProductDTO {
     private double precio;
     private int stock;
 
-    public ProductDTO() {}
-
-    public ProductDTO(Long id, String nombre, String descripcion, double precio, int stock) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.stock = stock;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-
-    public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
-
-    public double getPrecio() { return precio; }
-    public void setPrecio(double precio) { this.precio = precio; }
-
-    public int getStock() { return stock; }
-    public void setStock(int stock) { this.stock = stock; }
 }
 
 
