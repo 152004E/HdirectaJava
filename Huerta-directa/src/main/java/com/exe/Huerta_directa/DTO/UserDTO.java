2571256c89c165d826426b3798397a8f51bedca7
Santiago <santipuentes2017@gmail.com>
2025-09-15 17:40:41 -0500

    Santiago: adding some things

--- Huerta-directa/src/main/java/com/exe/Huerta_directa/DTO/UserDTO.java
+++ Huerta-directa/src/main/java/com/exe/Huerta_directa/DTO/UserDTO.java
@@ -19,29 +19,4 @@ public class UserDTO {
     private String rol;
     private boolean activo;
 
-
-    public UserDTO() {}
-
-
-    public UserDTO(Long id, String nombres, String apellidos, String email, String rol, boolean activo) {
-        this.id = id;
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.email = email;
-        this.rol = rol;
-        this.activo = activo;
-    }
-
-    public void setId(Long id) { this.id = id; }
-
-    public void setNombres(String nombres) { this.nombres = nombres; }
-
-    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
-
-    public void setEmail(String email) { this.email = email; }
-
-    public void setRol(String rol) { this.rol = rol; }
-
-    public void setActivo(boolean activo) { this.activo = activo; }
 }
-
