8cb05ac5d595431b2f23b7208e03605f1560f1fb
Emerson Reyes <reyesemerson643@gmail.com>
2025-09-22 22:29:14 -0500

    fixing dependencias

--- Huerta-directa/src/main/java/com/exe/Huerta_directa/Controllers/UserController.java
+++ Huerta-directa/src/main/java/com/exe/Huerta_directa/Controllers/UserController.java
@@ -8,7 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
