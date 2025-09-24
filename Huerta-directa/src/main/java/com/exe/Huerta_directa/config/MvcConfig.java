4b53e7e506ce739c17506ac13964d3e897398341
Emerson Reyes <reyesemerson643@gmail.com>
2025-09-24 14:49:44 -0500

    Changes

--- Huerta-directa/src/main/java/com/exe/Huerta_directa/config/MvcConfig.java
+++ Huerta-directa/src/main/java/com/exe/Huerta_directa/config/MvcConfig.java
@@ -5,20 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-      // Aquí traes la propiedad que definiste en application.properties
+    // Aquí traes la propiedad que definiste en application.properties
     @Value("${upload.path}")
     private String uploadPath;
+
     @Override
-     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Se asigna la URL pública /images/productos/** para que apunte a la carpeta local
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Se asigna la URL pública /images/productos/** para que apunte a la carpeta
+        // local
         registry.addResourceHandler("/images/productos/**")
                 .addResourceLocations("file:///" + uploadPath + "/productos/");
     }
-
 
 }
