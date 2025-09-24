f2c903b45d670ce5f6ef44d0a4488a8829980c1b
Santiago <santipuentes2017@gmail.com>
2025-09-24 17:00:59 -0500

    Santiago: addin controller detalis producto for update that information of prodcuts

---
+++ Huerta-directa/src/main/java/com/exe/Huerta_directa/Controllers/ProductViewController.java
@@ -0,0 +1,26 @@
+package com.exe.Huerta_directa.Controllers;
+
+import com.exe.Huerta_directa.DTO.ProductDTO;
+import com.exe.Huerta_directa.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ProductViewController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/producto/{id}")
+    public String verProducto(@PathVariable("id") Long id, Model model) {
+        ProductDTO producto = productService.obtenerProductPorId(id);
+        if (producto == null) {
+            return "error/404"; // Página de error
+        }
+        model.addAttribute("producto", producto);
+        return "Productos/product_detail";
+    }
+}
