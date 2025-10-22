6ff26111317fb29cf904a1ea18da3571f87998ac
Emerson Reyes <reyesemerson643@gmail.com>
2025-10-21 20:26:23 -0500

    Implementacion de comments

--- Huerta-directa/src/main/java/com/exe/Huerta_directa/Impl/CommentServiceImpl.java
+++ Huerta-directa/src/main/java/com/exe/Huerta_directa/Impl/CommentServiceImpl.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.yaml.snakeyaml.events.CommentEvent;
 
 import com.exe.Huerta_directa.DTO.CommentDTO;
+import com.exe.Huerta_directa.DTO.ProductDTO;
 import com.exe.Huerta_directa.Entity.Comment;
+import com.exe.Huerta_directa.Entity.Product;
+import com.exe.Huerta_directa.Entity.User;
 import com.exe.Huerta_directa.Repository.CommentRepository;
 import com.exe.Huerta_directa.Repository.ProductRepository;
 import com.exe.Huerta_directa.Repository.UserRepository;
@@ -16,13 +20,13 @@ import com.exe.Huerta_directa.Service.CommentService;
 public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
     private final UserRepository userRepository;
-    private final ProductRepository productRepository;
+    // private final ProductRepository productRepository;
 
     public CommentServiceImpl(CommentRepository commentRepository, UserRepository userRepository,
             ProductRepository productRepository) {
         this.commentRepository = commentRepository;
         this.userRepository = userRepository;
-        this.productRepository = productRepository;
+        // this.productRepository = productRepository;
     }
 
     // 🔹 Listar todos los comentarios
@@ -35,18 +39,34 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public CommentDTO obtenerCommentPorId(Long idComment) {
-        return null;
+        Comment comment = commentRepository.findById(idComment)
+                .orElseThrow(() -> new RuntimeException("Comentario no encontrado con el id " + idComment));
+
+        return convertirADTO(comment);
     }
 
     // 🔹 Crear un nuevo comentario (relacionado con usuario y/o producto)
     @Override
     public CommentDTO crearComment(CommentDTO commentDTO, Long userId, Long productId) {
-        return null;
+        // buscar el usuario
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado con el id " + userId));
+
+        // Covertir el comentario a entidad
+        Comment comment = convertirAEntity(commentDTO);
+        comment.setUser(user); //le asigamos el comentario al user
+
+        Comment newComment = commentRepository.save(comment);
+
+        return convertirADTO(newComment);
     }
 
     // 🔹 Actualizar un comentario existente
     @Override
     public CommentDTO actualizarComment(Long idComment, CommentDTO commentDTO) {
+     //   Comment commmentExistente = commentRepository.findById(idComment).orElseThrow(()-> new RuntimeException("Comentario no encontrado con el id "+ idComment));
+
+
         return null;
     }
 
@@ -93,7 +113,7 @@ public class CommentServiceImpl implements CommentService {
         return commentDTO;
 
     }
-    /* 
+
     private Comment convertirAEntity(CommentDTO commentDTO) {
         Comment commentEntity = new Comment();
         commentEntity.setIdComment(commentDTO.getIdComment());
@@ -102,12 +122,27 @@ public class CommentServiceImpl implements CommentService {
         commentEntity.setCreationComment(commentDTO.getCreationComment());
         commentEntity.setNameCommenter(commentDTO.getNameCommenter());
         commentEntity.setUser(userRepository.findById(commentDTO.getUserId()).orElse(null));
-        commentEntity.setProduct(productRepository.findById(commentDTO.getProductId()).orElse(null));
-
-      
+        // commentEntity.setProduct(productRepository.findById(commentDTO.getProductId()).orElse(null));
 
         return commentEntity;
-
     }
-    */
+    // Actualizar Entity con datos del DTO
+    /* 
+    private void actualizarDatosProducto(Product product, ProductDTO productDTO) {
+        product.setNameProduct(productDTO.getNameProduct());
+        product.setPrice(productDTO.getPrice());
+        product.setCategory(productDTO.getCategory());
+        product.setImageProduct(productDTO.getImageProduct());
+        product.setUnit(productDTO.getUnit());
+        product.setDescriptionProduct(productDTO.getDescriptionProduct());
+        product.setPublicationDate(productDTO.getPublicationDate());
+
+        if (productDTO.getUserId() != null) {
+            User user = userRepository.findById(productDTO.getUserId())
+                    .orElseThrow(() -> new RuntimeException("Usuario no encontrado con id: " + productDTO.getUserId()));
+            product.setUser(user);
+        }
+    }
+        */
+
 }
