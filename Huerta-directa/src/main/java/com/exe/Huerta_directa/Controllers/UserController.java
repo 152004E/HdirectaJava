ca4ca87dc7ae7999c77773f5882d3be8f4784b2b
Santiago <santipuentes2017@gmail.com>
2025-11-11 18:51:41 -0500

    santiago

--- Huerta-directa/src/main/java/com/exe/Huerta_directa/Controllers/UserController.java
+++ Huerta-directa/src/main/java/com/exe/Huerta_directa/Controllers/UserController.java
@@ -30,7 +30,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
