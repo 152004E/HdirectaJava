98d1f808b89866e7b077de47e345b1b3e1d84875
Emerson Reyes <reyesemerson643@gmail.com>
2026-02-24 17:39:53 -0500

    Login terminado

--- huerta-directa-front/src/components/GlobalComponents/Background.tsx
+++ huerta-directa-front/src/components/GlobalComponents/Background.tsx
@@ -1,8 +1,8 @@
 export const Background = () => {
   return (
     <>
-      <div className="absolute -top-20 -left-32 w-96 h-96 bg-[#8dc84b] rounded-full blur-3xl opacity-40"></div>
-      <div className="absolute top-20 -right-40 w-112.5 h-112.5 bg-[#5aaa37] rounded-full blur-3xl opacity-30"></div>
+      <div className="absolute -top-20 -left-32 w-96 h-96 bg-[#8dc84b] rounded-full blur-3xl opacity-30"></div>
+      <div className="absolute top-20 -right-40 w-112.5 h-112.5 bg-[#8dc84b] rounded-full blur-3xl opacity-30"></div>
     </>
   );
 };
---
+++ huerta-directa-front/src/components/GlobalComponents/PasswordInput.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLock,
+  faEye,
+  faEyeSlash,
+} from "@fortawesome/free-solid-svg-icons";
+
+interface PasswordInputProps {
+  name: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  required?: boolean;
+}
+
+export const PasswordInput: React.FC<PasswordInputProps> = ({
+  name,
+  value,
+  onChange,
+  placeholder = "Contraseña",
+  required = false,
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="w-full mb-2">
+      <div className="flex items-center min-w-[320px] relative">
+        
+        {/* 🔒 Icono izquierdo */}
+        <div className="absolute left-3 text-[#888]">
+          <FontAwesomeIcon icon={faLock} />
+        </div>
+
+        {/* Input */}
+        <input
+          type={showPassword ? "text" : "password"}
+          name={name}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          required={required}
+          className="py-2.5 pl-10 pr-12 w-full my-1.5 border-2 border-[#8dc84b] rounded-[15px] outline-none text-base text-[#333128] transition-all duration-300 focus:border-[#004d00] focus:shadow-[0_0_8px_rgba(0,77,0,0.4)]"
+        />
+
+        {/* 👁 Icono derecho dinámico */}
+        <div className="absolute right-4 text-[#888] cursor-pointer hover:text-[#004d00] transition-colors">
+          <FontAwesomeIcon
+            icon={showPassword ? faEyeSlash : faEye}
+            onClick={() => setShowPassword(!showPassword)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
